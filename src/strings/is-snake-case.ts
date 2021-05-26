a362e40fbd1a1bc086fbd549ccb5d05f537a3295
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-05-26 17:40:17 +0800

    refactor: is-snake-case

--- src/strings/is-snake-case.ts
+++ src/strings/is-snake-case.ts
@@ -2,18 +2,17 @@ type Lower = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "
 
 type IsSnakeCaseImpl<T extends string, U extends 0 | 1 = 0> =
     T extends `${infer Head}${infer Tail}`
-        ? U extends 0
-            ? Head extends Lower
+        ? {
+            0: Head extends Lower
                 ? IsSnakeCaseImpl<Tail, 1>
-                : false
-            : Head extends "_"
-                ? Tail extends ""
-                    ? false
-                    : IsSnakeCaseImpl<Tail, 0>
-                : Head extends Lower
-                    ? IsSnakeCaseImpl<Tail, 1>
-                    : false
-    : true
+                : false;
+            1: Head extends "_"
+                ? IsSnakeCaseImpl<Tail, 0>
+                : IsSnakeCaseImpl<T, 0>;
+        }[
+            Tail extends "" ? 0 : U // U === 1 -> allow underscore character
+        ]
+        : true
 ;
 
 export type IsSnakeCase<T extends string> =
