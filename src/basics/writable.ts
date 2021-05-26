dadfa9423b9d5285941cec6272b19d8b81f1e991
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-05-26 14:29:31 +0800

    feat: writable

---
+++ src/basics/writable.ts
@@ -0,0 +1,5 @@
+export type Writable<T> =
+    {
+        -readonly [K in keyof T]: T[K];
+    }
+;
---
+++ src/basics/writable.typetest.ts
@@ -0,0 +1,18 @@
+import { Writable } from "./writable"
+
+export {};
+
+interface Todo1 {
+    readonly title: string
+    readonly description: string
+    readonly completed: boolean
+}
+
+declare let todo: Todo1;
+
+// @ts-expect-error
+todo.title = "a";
+
+declare let writableTodo: Writable<Todo1>;
+
+writableTodo.title = "a";
