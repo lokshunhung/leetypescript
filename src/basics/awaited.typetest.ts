6a194e1351598819a4334f3d3a58275bb9b86c59
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-05-26 15:08:51 +0800

    feat: awaited

---
+++ src/basics/awaited.typetest.ts
@@ -0,0 +1,34 @@
+import { Equal, Expect } from "@type-challenges/utils";
+import { Awaited } from "./awaited";
+
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+
+type TestCases = [
+    Expect<Equal<
+        Awaited<X>,
+        string
+    >>,
+
+    Expect<Equal<
+        Awaited<Y>,
+        { field: number }
+    >>,
+
+    Expect<Equal<
+        Awaited<number>,
+        number
+    >>,
+
+    // While technically this case is not possible,
+    // typescript allows such types to be constructed.
+    // IMO the `Awaited` type should have this covered,
+    // so let's leave this here until future typescript
+    // versions decide to break it and this will be removed.
+    //
+    // Note: Use `type Awaited<T> = T extends Promise<infer U> ? U : T;`
+    Expect<Equal<
+        Awaited<Promise<Promise<boolean>>>,
+        boolean
+    >>,
+];
