e94f1ee743775ef542fc32254a39cb393afe1dc4
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-05-27 15:21:14 +0800

    refactor: first-of-array-strict use jump table

--- src/arrays/first-of-array-strict.ts
+++ src/arrays/first-of-array-strict.ts
@@ -1,19 +1,38 @@
-type FirstStrictImpl<T extends any[]> =
-    T extends [infer Head, ...any[]]
-        ? Head
-        : T extends [...infer Init, infer Last]
-            ? Init extends [...any[], any]
-                ? FirstStrictImpl<Init>
-                : FirstStrictImpl<Init> | Last
-            : T[number]
-;
+export type FirstStrict<
+    T extends any[],
+    U extends 0 | 1 = 0 // U === 1 -> has performed undefined check
+> =
+    {
+        0:
+        never
+        ;
 
-export type FirstStrict<T extends any[]> =
-    T extends []
-        ? never
-        : T extends [any, ...any[]]
-            ? FirstStrictImpl<T>
-            : T extends [...any[], any]
-                ? FirstStrictImpl<T>
-                : T[number] | undefined
+        1:
+        T extends [infer Head, ...any[]]
+            ? Head
+            : never
+        ;
+
+        2:
+        T extends [...infer Init, infer Last]
+            ? Init extends [...any[], any]
+                ? FirstStrict<Init, 1>
+                : FirstStrict<Init, 1> | Last
+            : never
+        ;
+
+        3:
+        FirstStrict<T, 1> | undefined
+        ;
+
+        4:
+        T[number]
+        ;
+    }[
+        T extends [] ? 0 :
+        T extends [any, ...any[]] ? 1 :
+        T extends [...any[], any] ? 2 :
+        U extends 0 ? 3 :
+        4
+    ]
 ;
