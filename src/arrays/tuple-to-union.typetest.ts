c2bf3b24bffbc1d19188b98ed5a1d2158687555f
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-05-27 14:21:52 +0800

    feat: tuple-to-union

---
+++ src/arrays/tuple-to-union.typetest.ts
@@ -0,0 +1,16 @@
+import { Equal, Expect } from "@type-challenges/utils";
+import { TupleToUnion } from "./tuple-to-union";
+
+export {};
+
+type TestCases = [
+    Expect<Equal<
+        TupleToUnion<[123, '456', true]>,
+        123 | '456' | true
+    >>,
+
+    Expect<Equal<
+        TupleToUnion<[123]>,
+        123
+    >>,
+];
