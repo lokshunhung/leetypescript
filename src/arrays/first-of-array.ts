8f24ccff61da0ce955cd46a9a06a127dcbd4c7ba
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-05-27 14:57:45 +0800

    fix: first-of-array handling spread types

--- src/arrays/first-of-array.ts
+++ src/arrays/first-of-array.ts
@@ -1,7 +1,9 @@
-// https://github.com/type-challenges/type-challenges/blob/d2b241a380fb09b54fe3b41d7af4f7de0bea26ef/questions/14-easy-first/README.md
-
 export type First<T extends any[]> =
     T extends [infer Head, ...any[]]
         ? Head
-        : never
+        : T extends [...infer Init, infer Last]
+            ? Init extends [...any[], any]
+                ? First<Init>
+                : First<Init> | Last
+            : T[number]
 ;
--- src/arrays/first-of-array.typetest.ts
+++ src/arrays/first-of-array.typetest.ts
@@ -1,26 +1,63 @@
-import { Equal, Expect } from "@type-challenges/utils";
+import { Equal, Expect, NotEqual } from "@type-challenges/utils";
 import { First } from "./first-of-array";
 
 export {};
 
 type TestCases = [
     Expect<Equal<
-        First<[3, 2, 1]>,
-        3
-    >>,
-
-    Expect<Equal<
-        First<[() => 123, { a: string }]>,
-        () => 123
-    >>,
-
-    Expect<Equal<
         First<[]>,
         never
     >>,
+    Expect<Equal<
+        First<[3, 2, 1]>,
+        3
+    >>,
+    Expect<Equal<
+        First<string[]>,
+        string
+    >>,
 
     Expect<Equal<
-        First<[undefined]>,
-        undefined
+        First<[boolean, ...number[]]>,
+        boolean
+    >>,
+    Expect<Equal<
+        First<[...number[], string]>,
+        number | string
+    >>,
+    Expect<Equal<
+        First<[boolean, ...number[], string]>,
+        boolean
+    >>,
+
+    Expect<Equal<
+        First<[boolean, () => void, ...number[]]>,
+        boolean
+    >>,
+    Expect<Equal<
+        First<[...number[], () => void, string]>,
+        number | (() => void)
+    >>,
+    Expect<Equal<
+        First<[boolean, () => void, ...number[], () => void, string]>,
+        boolean
+    >>,
+
+    Expect<Equal<
+        First<[string | symbol, number, boolean]>,
+        string | symbol
+    >>,
+    Expect<Equal<
+        First<[...Array<string | symbol>, number, boolean]>,
+        number | string | symbol
+    >>,
+
+    Expect<NotEqual<
+        First<[string | symbol, number, boolean]>,
+        string
+    >>,
+    Expect<NotEqual<
+        First<[...Array<string | symbol>, number, boolean]>,
+        string | symbol
     >>,
 ];
