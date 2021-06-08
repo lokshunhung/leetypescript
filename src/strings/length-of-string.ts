f2f6c9c3fd7bb58cd14936c5ff473c3ae3e3e6a5
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-08 10:45:57 +0800

    chore: length-of-string type-challenges link

--- src/strings/length-of-string.ts
+++ src/strings/length-of-string.ts
@@ -1,3 +1,5 @@
+// https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/298-medium-length-of-string/README.md
+
 type LengthImpl<T extends string, I extends any[] = []> =
     T extends `${infer _}${infer Tail}`
         ? LengthImpl<Tail, [any, ...I]>
