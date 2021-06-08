93261721fed5ac322bdc5caee4bdce23209ca53e
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-08 18:23:54 +0800

    chore: chainable-options link

--- src/functions/chainable-options.ts
+++ src/functions/chainable-options.ts
@@ -1,3 +1,5 @@
+// https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/12-medium-chainable-options/README.md
+
 export type Chainable<T extends Record<string, any> = {}> = {
     option<K extends string, V extends any>(key: K, value: V): Chainable<Omit<T, K> & Record<K, V>>;
     get(): T;
