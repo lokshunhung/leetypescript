d83333f41939993f5e73f1e56bc8e38c1e77ebee
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-08 17:53:26 +0800

    refactor: get-readonly-keys alternative

--- src/objects/get-readonly-keys.ts
+++ src/objects/get-readonly-keys.ts
@@ -1,5 +1,6 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/5-extreme-readonly-keys/README.md
 
+// Also: `type GetReadonlyKeys<T, K = keyof T> = K extends keyof T ? Equal<Pick<T, K>, Pick<Readonly<T>, K>> extends true ? K : never : never;` with the `Pick`, `Readonly` and `Equal` helper
 export type GetReadonlyKeys<T> =
     {
         [K in keyof T]-?:
