93ad541968fb1ffa005e493f7c42e02005f2c426
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-08 19:19:32 +0800

    test: additional test for parse-query-string

--- src/strings/parse-query-string.typetest.ts
+++ src/strings/parse-query-string.typetest.ts
@@ -44,4 +44,54 @@ type TestCases = [
         ParseQueryString<"k1=v1&k1=v1">,
         { k1: "v1" }
     >>,
+
+    // Additional tests (leading boolean)
+    Expect<Equal<
+        ParseQueryString<"k1&k2=v2">,
+        { k1: true; k2: "v2" }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k1&k1&k2=v2">,
+        { k1: true; k2: "v2" }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k1&k2=v2&k2=v2">,
+        { k1: true; k2: "v2" }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k1&k1&k2=v2&k2=v3">,
+        { k1: true; k2: ["v2", "v3"] }
+    >>,
+
+    // Additional tests (trailing boolean)
+    Expect<Equal<
+        ParseQueryString<"k2=v3&k2=v2&k1&k1">,
+        { k1: true; k2: ["v3", "v2"] }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k2=v2&k2=v2&k1">,
+        { k1: true; k2: "v2" }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k2=v2&k1&k1">,
+        { k1: true; k2: "v2" }
+    >>,
+
+    // Additional tests (sandwiched boolean)
+    Expect<Equal<
+        ParseQueryString<"k1&k2=v2&k1">,
+        { k1: true; k2: "v2" }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k1&k2=v2&k2=v3&k1">,
+        { k1: true; k2: ["v2", "v3"] }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k2=v2&k1&k1&k2=v2">,
+        { k1: true; k2: "v2" }
+    >>,
+    Expect<Equal<
+        ParseQueryString<"k2=v2&k1&k1&k2=v3">,
+        { k1: true; k2: ["v2", "v3"] }
+    >>,
 ];
