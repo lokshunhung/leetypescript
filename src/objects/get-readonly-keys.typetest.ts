2bfe65017a02e9811f034015db858abaa8821355
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-08 18:11:53 +0800

    refactor: get-readonly-keys test

--- src/objects/get-readonly-keys.typetest.ts
+++ src/objects/get-readonly-keys.typetest.ts
@@ -15,6 +15,12 @@ interface Todo2 {
     completed?: boolean;
 }
 
+interface Todo3 {
+    readonly foo: undefined;
+    readonly bar: null;
+    baz?: string
+}
+
 type TestCases = [
     Expect<Equal<
         GetReadonlyKeys<Todo1>,
@@ -23,5 +29,9 @@ type TestCases = [
     Expect<Equal<
         GetReadonlyKeys<Todo2>,
         "title" | "description"
+    >>,
+    Expect<Equal<
+        GetReadonlyKeys<Todo3>,
+        "foo" | "bar"
     >>,
 ];
