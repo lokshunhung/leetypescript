d79541e996e7a5c8962f2f646fb51b93d549b4ab
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-09 14:36:13 +0800

    feat: add alternative impl for join

--- src/strings/join.ts
+++ src/strings/join.ts
@@ -17,3 +17,20 @@ type Join<
 export declare function join<Del extends string>(delimiter: Del):
     <Arr extends string[]>(...parts: Arr) => Join<Del, Arr>
 ;
+
+/*
+Alternative implementation for Join that starts working from the end
+type Join<
+    Del extends string,
+    Arr extends string[],
+    Res extends string = ""
+> =
+    Arr extends [...infer Init, infer Last]
+        ? Last extends string
+            ? Init extends string[]
+                ? Join<Del, Init, `${Last}${Res extends "" ? "" : Del}${Res}`>
+                : never
+            : never
+        : Res
+;
+*/
