44c5388b59aaf7babcf07a537d59fd4b551044c9
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-09 11:34:04 +0800

    feat: wip slice handle negative start index

--- src/arrays/slice.typetest.ts
+++ src/arrays/slice.typetest.ts
@@ -5,39 +5,38 @@ type Slice<
 
     // Jump
     // 0: low bound; skip value;
-    //               goto 1 if `Itr.length` === `Start` && `End` > 0
-    //               goto 2 if `Itr.length` === `Start` && `End` < 0
-    // 1: in bound; take value; goto 3 if `Itr.length` === `End`
-    // 2: in bound; take value; goto 3 if `Tail.length` === -`End`
-    // 3: out bound; return `Res`
-    Jump extends 0 | 1 | 2 | 3 = 0,
+    //               goto 1 if `Itr.length` === `Start` && `Start` >= 0
+    //               goto 1 if `Tail.length` === -`Start` && `Start` < 0
+    // 1: in bound;  take value;
+    //               goto 2 if `Itr.length` === `End` && `End` >= 0
+    //               goto 2 if `Tail.length` === -`End` && `End` < 0
+    // 2: out bound; return `Res`
+    Jump extends 0 | 1 | 2 = 0,
     Itr extends 1[] = [],
     Res extends any[] = []
 > = Arr extends [infer Head, ...infer Tail]
     ? {
         0:
-        Itr["length"] extends Start
-            ? `${End}` extends `-${number}`
-                ? Slice<Arr, Start, End, 2, Itr, Res>
-                : Slice<Arr, Start, End, 1, Itr, Res>
-            : Slice<Tail, Start, End, 0, [1, ...Itr], Res>
+        `${Start}` extends `-${infer _Start}`
+            ? `${Tail["length"]}` extends _Start
+                ? Slice<Tail, Start, End, 1, Itr, Res>
+                : Slice<Tail, Start, End, 0, [1, ...Itr], Res>
+            : Itr["length"] extends Start
+                ? Slice<Arr, Start, End, 1, Itr, Res>
+                : Slice<Tail, Start, End, 0, [1, ...Itr], Res>
         ;
 
         1:
-        Itr["length"] extends End
-            ? Slice<Arr, Start, End, 3, Itr, Res>
-            : Slice<Tail, Start, End, 1, [1, ...Itr], [...Res, Head]>
+        `${End}` extends `-${infer _End}`
+            ? `${Tail["length"]}` extends _End
+                ? Slice<Arr, Start, End, 2, Itr, [...Res, Head]>
+                : Slice<Tail, Start, End, 1, [1, ...Itr], [...Res, Head]>
+            : Itr["length"] extends End
+                ? Slice<Arr, Start, End, 2, Itr, Res>
+                : Slice<Tail, Start, End, 1, [1, ...Itr], [...Res, Head]>
         ;
 
         2:
-        `${End}` extends `-${infer _End}`
-            ? `${Tail["length"]}` extends _End
-                ? Slice<Arr, Start, End, 3, Itr, [...Res, Head]>
-                : Slice<Tail, Start, End, 2, [1, ...Itr], [...Res, Head]>
-            : never
-        ;
-
-        3:
         Res
         ;
     }[Jump]
@@ -47,6 +46,8 @@ type Slice<
 import { Equal, Expect } from "@type-challenges/utils";
 
 type Arr = [1, 2, 3, 4, 5];
+
+type _ = Slice<Arr, -3, -1>
 
 type TestCases = [
     // basic
