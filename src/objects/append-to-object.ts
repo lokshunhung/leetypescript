d3923f65e83b402d0e6c611aa318df7c3633d48e
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-10 10:06:47 +0800

    refactor: append-to-object use flatten intersection

--- src/objects/append-to-object.ts
+++ src/objects/append-to-object.ts
@@ -1,13 +1,8 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/527-medium-append-to-object/README.md
 
+// Also: `type AppendToObject<T, U extends string | number | symbol, V> = { [K in (keyof T) | U]: K extends U ? V : K extends keyof T ? T[K] : never };`
 export type AppendToObject<T, U extends string | number | symbol, V> =
-    {
-        [K in (keyof T) | U]:
-            K extends U
-                ? V
-                : K extends keyof T
-                    ? T[K]
-                    : never
-        ;
-    }
+    T & Record<U, V> extends infer R
+        ? { [K in keyof R]: R[K] } // flatten intersection
+        : never
 ;
