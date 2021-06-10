45fd6299f44907f4464e7fd21ec7f0dc87ebdab1
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-10 11:51:25 +0800

    refactor: length-of-string iterate by 1[]

--- src/strings/length-of-string.ts
+++ src/strings/length-of-string.ts
@@ -1,8 +1,8 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/298-medium-length-of-string/README.md
 
-type LengthImpl<T extends string, I extends any[] = []> =
+type LengthImpl<T extends string, I extends 1[] = []> =
     T extends `${string}${infer Tail}`
-        ? LengthImpl<Tail, [any, ...I]>
+        ? LengthImpl<Tail, [1, ...I]>
         : I["length"]
 ;
 
