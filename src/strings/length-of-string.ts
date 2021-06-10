73f15021d2d58bb9e7c940ee52286a991061ebb2
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-10 11:45:39 +0800

    refactor: length-of-string remove useless infer placeholder

--- src/strings/length-of-string.ts
+++ src/strings/length-of-string.ts
@@ -1,7 +1,7 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/298-medium-length-of-string/README.md
 
 type LengthImpl<T extends string, I extends any[] = []> =
-    T extends `${infer _}${infer Tail}`
+    T extends `${string}${infer Tail}`
         ? LengthImpl<Tail, [any, ...I]>
         : I["length"]
 ;
