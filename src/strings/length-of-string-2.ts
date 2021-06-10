0f4d794f568bc9359fb5d78c6235188b6fa23b3b
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-10 11:50:21 +0800

    refactor: length-of-string-2 hide impl

--- src/strings/length-of-string-2.ts
+++ src/strings/length-of-string-2.ts
@@ -1,9 +1,13 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/651-hard-length-of-string-2/README.md
 
-export type Length2<T extends string, I extends 1[] = []> =
+type Length2Impl<T extends string, I extends 1[] = []> =
     T extends `${string}${string}${string}${string}${string}${string}${string}${string}${string}${string}${infer T}`
-        ? Length2<T, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ...I]>
+        ? Length2Impl<T, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ...I]>
         : T extends `${string}${infer T}`
-            ? Length2<T, [1, ...I]>
+            ? Length2Impl<T, [1, ...I]>
             : I["length"]
+;
+
+export type Length2<T extends string> =
+    Length2Impl<T>
 ;
