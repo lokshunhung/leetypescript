9cc293e7a75b720dea69cdc6b74a6860057332d4
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-10 10:07:44 +0800

    refactor: append-to-object use built-in PropertyKey

--- src/objects/append-to-object.ts
+++ src/objects/append-to-object.ts
@@ -1,7 +1,8 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/527-medium-append-to-object/README.md
 
+// Note: `string | number | symbol` is the same as the built-in `PropertyKey` type
 // Also: `type AppendToObject<T, U extends string | number | symbol, V> = { [K in (keyof T) | U]: K extends U ? V : K extends keyof T ? T[K] : never };`
-export type AppendToObject<T, U extends string | number | symbol, V> =
+export type AppendToObject<T, U extends PropertyKey, V> =
     T & Record<U, V> extends infer R
         ? { [K in keyof R]: R[K] } // flatten intersection
         : never
