a11149f26bb08dc7d8eac7f520e7f87600201a39
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-10 11:45:57 +0800

    refactor: length-of-string-2 remove useless infer placeholder

--- src/strings/length-of-string-2.ts
+++ src/strings/length-of-string-2.ts
@@ -1,9 +1,9 @@
 // https://github.com/type-challenges/type-challenges/blob/84360326c02461ae63b55c6d396ad1679917c430/questions/651-hard-length-of-string-2/README.md
 
 export type Length2<T extends string, I extends 1[] = []> =
-    T extends `${infer H1}${infer H2}${infer H3}${infer H4}${infer H5}${infer H6}${infer H7}${infer H8}${infer H9}${infer H10}${infer T}`
+    T extends `${string}${string}${string}${string}${string}${string}${string}${string}${string}${string}${infer T}`
         ? Length2<T, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ...I]>
-        : T extends `${infer H}${infer T}`
+        : T extends `${string}${infer T}`
             ? Length2<T, [1, ...I]>
             : I["length"]
 ;
