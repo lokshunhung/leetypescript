4e348a2b1a7d6fe2a9fbe7b57f7e20a0cf46d3ef
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-11 12:11:42 +0800

    refactor: tuple-to-enum use TupleIndex

--- src/objects/tuple-to-enum-object.ts
+++ src/objects/tuple-to-enum-object.ts
@@ -20,3 +20,20 @@ type EnumImpl<
 export type Enum<T extends readonly string[], N extends boolean = false> =
     EnumImpl<T, N>
 ;
+
+/*
+Alternative implementation for better readability (https://github.com/type-challenges/type-challenges/issues/492)
+type TupleIndex<T extends readonly any[]> =
+    T extends readonly [any, ...infer Tail]
+        ? TupleIndex<Tail> | Tail["length"]
+        : never
+;
+
+export type Enum<T extends readonly string[], N extends boolean = false> =
+    {
+        readonly [K in TupleIndex<T> as Capitalize<T[K]>]:
+            N extends true ? K : T[K]
+        ;
+    }
+;
+*/
